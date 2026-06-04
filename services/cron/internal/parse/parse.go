e2c9a40f41870429a840766eb810fd4baf57fe15
Mark <mikin.mark@lit1533.ru>
2026-06-04 11:39:09 +0300

    обработка текста новости

--- services/cron/internal/parse/parse.go
+++ services/cron/internal/parse/parse.go
@@ -3,13 +3,17 @@ package parse
 import (
 	"bytes"
 	"fmt"
+	"html"
 	"io"
 	"log"
 	"net/http"
+	"regexp"
 	"smartFlow/internal/models"
 	"strings"
 
 	"github.com/SlyMarbo/rss/v2"
+	"github.com/forPelevin/gomoji"
+	"github.com/microcosm-cc/bluemonday"
 	"gorm.io/gorm"
 )
 
@@ -77,26 +81,91 @@ func rssParsing(db *gorm.DB, channel models.Channel, targetURL string) {
 		return
 	}
 
+	title := CleanChannelName(feed.Title)
+	re_title, err := CreateChannelRegex(title)
+	if err != nil {
+		fmt.Println("Ошибка:", err)
+		return
+	}
+	fmt.Printf("Название %s", title)
 	for i, item := range feed.Items {
 		if i >= 5 {
 			break
 		}
 
 		var link string
-		if len(item.Links) > 0 && item.Links[0] != nil {
-			link = item.Links[0].Href
-		}
-		if link == "" {
-			link = item.ID
-		}
-
+		link = item.ID
+		cleanBody := CleanTextWithGomoji(CleanHTML(item.Summary), re_title)
 		newsItem := models.News{
-			Body:        item.Title,
+			Body:        cleanBody,
 			MessageLink: link,
 			ChannelID:   channel.ID,
 		}
-
-		// Если новости нет — GORM запишет её. Если есть — просто пропустит.
 		db.Where(models.News{MessageLink: newsItem.MessageLink}).FirstOrCreate(&newsItem)
 	}
+}
+func CleanTextWithGomoji(input string, channelRe *regexp.Regexp) string {
+	if input == "" {
+		return ""
+	}
+
+	text := gomoji.RemoveEmojis(input)
+	reAllIcons := regexp.MustCompile(`[\p{So}\p{Pi}\p{Pf}]`)
+	text = reAllIcons.ReplaceAllString(text, "")
+
+	if channelRe != nil {
+		text = channelRe.ReplaceAllString(text, "")
+	}
+
+	reUsernames := regexp.MustCompile(`(^|\s|[,.!?;:])@([A-Za-z0-9_]+)`)
+	text = reUsernames.ReplaceAllString(text, "$1")
+
+	reSubscribe := regexp.MustCompile(`(?i)подписаться[\s!.,:-]*$`)
+	text = reSubscribe.ReplaceAllString(text, "")
+
+	reSpaces := regexp.MustCompile(`\s+`)
+	text = reSpaces.ReplaceAllString(text, " ")
+
+	return strings.TrimSpace(text)
+}
+func CleanHTML(htmlContent string) string {
+	p := bluemonday.StrictPolicy()
+
+	plainText := p.Sanitize(htmlContent)
+
+	return html.UnescapeString(plainText)
+}
+
+func CleanChannelName(name string) string {
+	name = strings.TrimSpace(name)
+
+	name = strings.TrimSuffix(name, " - Telegram Channel")
+	name = strings.TrimSuffix(name, " - Telegram channel")
+	name = strings.TrimSuffix(name, " - Telegram CHANNEL")
+
+	replacer := strings.NewReplacer(".", " ", "-", " ", "_", " ")
+	name = replacer.Replace(name)
+
+	reSpaces := regexp.MustCompile(`\s+`)
+	name = reSpaces.ReplaceAllString(name, " ")
+
+	return strings.TrimSpace(name)
+}
+func CreateChannelRegex(channelName string) (*regexp.Regexp, error) {
+	trimmed := strings.TrimSpace(channelName)
+	if trimmed == "" {
+		return nil, fmt.Errorf("название канала пустое")
+	}
+
+	words := strings.Fields(trimmed)
+
+	for i, word := range words {
+		words[i] = regexp.QuoteMeta(word)
+	}
+
+	pattern := strings.Join(words, `[\s\S]*?`)
+
+	finalPattern := "(?i)" + pattern + "."
+
+	return regexp.Compile(finalPattern)
 }
